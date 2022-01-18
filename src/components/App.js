2bca9086758e269a97b32a50cce61e1c3ac5c551
Hailey Magoline <magoline.hailey@gmail.com>
2022-01-17 20:25:06 -0600

    Completed readMe: "Connect a better premade action"

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from '../reducers';
-import { addOne } from '../actions/index';
+import { addOne, applyNumber } from '../actions/index';
 
 import './App.css';
 
@@ -13,6 +13,10 @@ function App() {
 
   const handleAddOne = () => {
     dispatch(addOne());
+  }
+
+  const handleApplyNumber = (number) => {
+    dispatch(applyNumber(number));
   }
 
   return (
@@ -38,21 +42,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton onClick={handleAddOne} value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick={() => handleApplyNumber(1)} value={1}/>
+              <CalcButton onClick={() => handleApplyNumber(2)} value={2}/>
+              <CalcButton onClick={() => handleApplyNumber(3)} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={() => handleApplyNumber(4)} value={4}/>
+              <CalcButton onClick={() => handleApplyNumber(5)} value={5}/>
+              <CalcButton onClick={() => handleApplyNumber(6)} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={() => handleApplyNumber(7)} value={7}/>
+              <CalcButton onClick={() => handleApplyNumber(8)} value={8}/>
+              <CalcButton onClick={() => handleApplyNumber(9)} value={9}/>
             </div>
 
             <div className="row">
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -19,6 +19,7 @@ const calculateResult = (num1, num2, operation) => {
 
 const reducer = (state, action) => {
     switch(action.type) {
+        
         case(ADD_ONE):
             return({
                 ...state,
